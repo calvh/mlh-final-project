fd68087a9eff453751dbe24605be8f0cd7eea687
Rebecca Li <Rebecca.CYLi@gmail.com>
2021-08-05 23:25:21 -0400

    Removed newline in rps.py

--- flask/app/rps.py
+++ flask/app/rps.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template
 
 rps = Blueprint("rps", __name__, template_folder="templates")
 
-
 @rps.route("/")
 def index():
     return render_template("index.html")
