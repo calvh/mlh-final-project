fecb79fded5eb919dea814e91d990c44cd3ae004
Rebecca Li <Rebecca.CYLi@gmail.com>
2021-08-06 16:50:50 -0400

    38-frontend, html/css for login/signup additions

--- flask/app/db.py
+++ flask/app/db.py
@@ -1,16 +1,30 @@
 import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# MONGODB_URI = (
+#     "mongodb://"
+#     "{user}:{password}@{hostname}:{port}/{db}?authSource={authSource}".format(
+#         user=os.environ.get("MONGO_USER"),
+#         password=os.environ.get("MONGO_PASSWORD"),
+#         hostname=os.environ.get("HOSTNAME"),
+#         port=27017,
+#         db=os.environ.get("DB"),
+#         authSource=os.environ.get("AUTH_SOURCE"),
+#     )
+# )
 
 MONGODB_URI = (
     "mongodb://"
-    "{user}:{password}@{hostname}:{port}/{db}?authSource={authSource}".format(
-        user=os.environ.get("MONGO_USER"),
-        password=os.environ.get("MONGO_PASSWORD"),
-        hostname=os.environ.get("HOSTNAME"),
+    "{hostname}:{port}/{db}".format(
+        hostname="localhost",
         port=27017,
-        db=os.environ.get("DB"),
-        authSource=os.environ.get("AUTH_SOURCE"),
+        db="DB",
+        authSource="admin",
     )
 )
+
 
 client = MongoClient(MONGODB_URI)
