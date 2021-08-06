f89d288de7f0e11d75da0c2e663bebbf4aaaccfe
Calvin Ho <18680207+calvh@users.noreply.github.com>
2021-08-06 00:03:16 -0400

    Add mongo integration to flask
    Removed models
    Removed Flask Migrate
    Removed SQL Alchemy references

---
+++ flask/app/db.py
@@ -0,0 +1,13 @@
+import os
+from pymongo import MongoClient
+
+MONGODB_URI = "mongodb://{user}:{password}@{hostname}:{port}/{db}?authSource={authSource}".format(
+    user=os.environ.get("MONGO_USER"),
+    password=os.environ.get("MONGO_PASSWORD"),
+    hostname=os.environ.get("HOSTNAME"),
+    port=27017,
+    db=os.environ.get("DB"),
+    authSource=os.environ.get("AUTH_SOURCE"),
+)
+
+client = MongoClient(MONGODB_URI)
