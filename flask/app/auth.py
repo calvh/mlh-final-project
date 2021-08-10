58ba94b11252c9d4e9e44922a3c51cba6ad8cf2d
Rebecca-CYLi <48325436+Rebecca-CYLi@users.noreply.github.com>
2021-08-10 18:30:58 -0400

    Update auth.py

--- flask/app/auth.py
+++ flask/app/auth.py
@@ -1,4 +1,12 @@
-from flask import ( Blueprint, flash, redirect, render_template, request, session, url_for)
+from flask import (
+    Blueprint,
+    flash,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import client
 from bson.json_util import dumps  # needed to serialize mongo object to json
