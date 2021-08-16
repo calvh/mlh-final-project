52b9b1fb6ffe467e46d2038a065c35cf1444d731
Rebecca Li <Rebecca.CYLi@gmail.com>
2021-08-16 16:39:21 -0400

    adding rps.py changes

--- flask/app/rps.py
+++ flask/app/rps.py
@@ -1,6 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session
+import json
+
+from app.db import client
 
 rps = Blueprint("rps", __name__, template_folder="templates")
+
+db = client["rps"]
+Users = db.users
 
 
 @rps.route("/")
@@ -16,3 +22,26 @@ def play():
 @rps.route("/stats")
 def stats():
     return render_template("stats.html")
+
+
+@rps.route("/scores", methods=["PUT"])
+def scores():
+    print("ABCDEVH")
+    print(request.get_data())
+    print(request.data)
+    print("getting JSON: ", request.get_json())
+
+    score_data = request.get_json()
+    print("scoredata: ", score_data)
+
+    if request.method == "PUT":
+        username = session["username"]
+        # insert/update in DB
+        Users.update_one(
+            {"username": username},
+            {"$set": {"gameScore": score_data}},
+            upsert=True
+        )
+    return json.dumps(
+        {"\nstatus": "OK", "user is": username, "\ngame-score": score_data}
+    )
